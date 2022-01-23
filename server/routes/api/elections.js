4baf6369dc9466fca9b2abc54e706d1e955c2d88
polea003 <polea003@fiu.edu>
2022-01-22 22:39:07 -0500

    initial commit

---
+++ server/routes/api/elections.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const mongodb = require('mongodb')
+const router = express.Router()
+
+//Get
+router.get('/', async (req, res) => {
+    const elections = await loadElectionsCollection()
+    res.send(await elections.find({}).toArray())
+})
+
+
+//Add
+router.post('/', async (req, res) => {
+    const elections = await loadElectionsCollection()
+    await elections.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    })
+    res.status(201).send()
+})
+
+//Delete
+router.delete('/:id', async (req, res) => {
+    const elections = await loadElectionsCollection()
+    await elections.deleteOne({_id: new mongodb.ObjectId(req.params.id)})
+    res.status(200).send()
+})
+
+async function loadElectionsCollection() {
+    const client = await mongodb.MongoClient.connect
+    ('mongodb+srv://panther123:panther123@panther-db.gfe61.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+        useNewUrlParser: true
+    })
+
+    return client.db('panther-db').collection('elections')
+}
+
+module.exports = router
