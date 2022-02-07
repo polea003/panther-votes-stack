7ea9db5ab851c2f27d5a06cd7ab0c5299d0dc5db
polea003 <polea003@fiu.edu>
2022-02-07 16:38:37 -0500

    devnet deploy sucessful

--- AnchorClient/programs/AnchorClient/src/lib.rs
+++ AnchorClient/programs/AnchorClient/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("Gu7xYmGthCWx6ZYSHhFy6AzDh1qEEmaKdQjMrZQxmYWD");
 
 #[program]
 pub mod anchor_client {
