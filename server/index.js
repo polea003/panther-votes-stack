4c060ea7db50e293615e7b7bf3a43321d50623fa
unknown <tylersalzberg@gmail.com>
2022-02-17 17:49:58 -0500

    Web Token added

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+
+connectDB()
 
 const app = express();
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 //Middleware
 app.use(bodyParser.json())
 app.use(cors())
--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router()
 const {registerUser, loginUser, GetMe} = require('../../controllers/userController')
 
-router.post('/', registerUser)
+const {protect} = require('../../middleware/authMiddleware')
+
+router.post('/user', registerUser)
 router.post('/login', loginUser)
-router.get('/me', GetMe)
+router.get('/me', protect, GetMe)
 
 module.exports = router
